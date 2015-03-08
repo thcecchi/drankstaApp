089ebe9bfd75934423240fb7fbdd32001324c6be
Tyler Cecchi <thcecchi@gmail.com>
2015-03-08 15:03:05 -0400

    minor code cleanup changes

--- app/common/googleService.js
+++ app/common/googleService.js
@@ -4,20 +4,6 @@
   angular.module('dranksta')
     .factory('GoogleService', function ($http, $rootScope, $log) {
 
-
-      // var googleInfo = {
-      //   // urlA: 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=bars+in+',
-      //   urlA: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=bar&key=AIzaSyD7V33_45D7vdPHFNKEasnNYXvr16P1Li4',
-      //
-      //   urlB: '&key=AIzaSyD7V33_45D7vdPHFNKEasnNYXvr16P1Li4',
-      // }
-      //
-      // var getGoogleInfo = function (locName) {
-      // // return $http.jsonp(googleInfo.urlA + locName + googleInfo.urlB);
-      // // return $http.get(googleInfo.urlA);
-      // console.log(googleInfo.urlA)
-      // };
-
       var map;
       var service;
       var infowindow;
@@ -28,12 +14,6 @@
         var coordinate=(locName).split(',');
         var latlng = new google.maps.LatLng(coordinate[0],coordinate[1]);
         console.log(latlng);
-
-
-        // parseInt(locName)
-        // console.log(locName)
-        // var locationName = new google.maps.LatLng(locName);
-        // console.log(locationName)
 
         map = new google.maps.Map(document.getElementById('map'), {
             center: latlng,
@@ -50,16 +30,6 @@
           .PlacesService(document.getElementById('main').appendChild(document.createElement('div')));
         service.textSearch(request, callback);
       }
-
-      // function callback(results, status) {
-      //   if (status == google.maps.places.PlacesServiceStatus.OK) {
-      //     for (var i = 0; i < results.length; i++) {
-      //       var place = results[i];
-      //       // createMarker(results[i]);
-      //       console.log(results[i])
-      //     }
-      //   }
-      // }
 
       var callback = function (results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
