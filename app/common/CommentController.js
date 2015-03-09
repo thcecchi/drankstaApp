6a79e056728eec2e868a6794f61ad9cb26e35d06
Tyler Cecchi <thcecchi@gmail.com>
2015-03-09 10:14:03 -0400

    rating and category creation are function, however the both values are rendered twice on the dom

--- app/common/CommentController.js
+++ app/common/CommentController.js
@@ -3,22 +3,53 @@
   angular.module("comment")
     .controller("CommentCtrl", function (CommentService, $scope, $http, $location, $routeParams) {
       var comCtrl = this;
+      var total = ''
+      var avg = ''
+
+        comCtrl.mode = function(store) {
+          console.log('fired!!')
+          var frequency = {};  // array of frequency.
+          var max = 0;  // holds the max frequency.
+          var result;   // holds the max frequency element.
+          for(var v in store) {
+            frequency[store[v]]=(frequency[store[v]] || 0)+1; // increment frequency.
+
+            if(frequency[store[v]] > max) { // is this frequency > max so far ?
+                max = frequency[store[v]];  // update max.
+                result = store[v];          // update result.
+            }
+          }
+
+          $('.category').append(result)
+          console.log(result)
+        };
+
 
       CommentService.getComments().success(function(data){
         comCtrl.comments=data; //declares data as items returned from server
         var totalArr = []
+        var catArr = []
         data.forEach(function(obj) {
-          if(obj.name == 'Husk') {
+          var barNamez = $('.barName').text()
+          console.log(barNamez)
+          if(obj.name == barNamez) {
             totalArr.push(obj.rating)
+            catArr.push(obj.category)
             console.log(totalArr)
+            console.log(catArr)
           }
-          var total = totalArr.reduce(function(a, b) {
-            return a + b;
-          });
 
-          var avg = total/totalArr.length
-          console.log(avg)
         })
+        total = totalArr.reduce(function(a, b) {
+          return a + b;
+        });
+
+        comCtrl.mode(catArr)
+
+        console.log(total)
+        avg = total/totalArr.length
+        console.log(avg)
+        $('.rating').append(avg)
       });
 
       CommentService.getOneComment($routeParams.itemId).success(function(data){
