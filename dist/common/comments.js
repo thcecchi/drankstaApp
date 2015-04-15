cddc2994d378f144aa30ccef4ff53a438645a1df
Tyler Cecchi <thcecchi@gmail.com>
2015-04-15 12:38:51 -0400

    pushing to gh-pages

---
+++ dist/common/comments.js
@@ -0,0 +1,12 @@
+(function(){
+  "use strict";
+  angular.module('comment', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider){
+    $routeProvider
+
+  });//end config
+
+
+})();
---
+++ dist/common/googleCtrl.js
@@ -0,0 +1,45 @@
+(function () {
+  "use strict";
+  angular.module("dranksta")
+    .controller("GoogleCtrl", function (GoogleService, $scope, $location, $routeParams) {
+
+      var goog = this;
+
+      goog.placeArr = GoogleService.barArr
+
+      goog.allBars = GoogleService.getAllBarz();
+
+      $scope.getTheBars = GoogleService.getAllBarz();
+
+      goog.getSingleBar = GoogleService.getOneBar($routeParams.barIndex);
+
+      $scope.location= "";
+        $scope.doSearch = function(){
+                if($scope.location === ''){
+                    alert('Directive did not update the location property in parent controller.');
+                } else {
+                    console.log($scope.location);
+                    var locCoords = parseInt($scope.location)
+
+                    console.log(locCoords);
+                    goog.locationInput($scope.location)
+                }
+
+            };
+
+
+      goog.locationInput = function (newLocation) {
+        var locName = newLocation
+        console.log(locName)
+        GoogleService.getBarInfo(locName)
+      }
+
+      $scope.renderBars = function() {
+        goog.allBars
+        console.log(goog.placeArr)
+      }
+
+  });
+
+
+})();
